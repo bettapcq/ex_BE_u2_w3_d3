89efcd27f739ba1884174f9ea5029f947d85a469
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-02-18 17:00:18 +0100

    update

--- src/main/java/bettapcq/exu2w3d3/runners/Runner.java
+++ src/main/java/bettapcq/exu2w3d3/runners/Runner.java
@@ -1,21 +1,38 @@
 package bettapcq.exu2w3d3.runners;
 
 import bettapcq.exu2w3d3.es1.AdapterClass;
+import bettapcq.exu2w3d3.es3.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Runner implements CommandLineRunner {
 
+    //es1:
     private final AdapterClass adapter;
+//es3:
 
-    public Runner(AdapterClass adapter) {
+    private final Tenente tenente;
+    private final Capitano capitano;
+    private final Maggiore maggiore;
+    private final Colonnello colonnello;
+    private final Generale generale;
+
+    public Runner(AdapterClass adapter, Tenente tenente, Capitano capitano, Maggiore maggiore, Colonnello colonnello, Generale generale) {
+        //es1:
         this.adapter = adapter;
+
+//es3:
+        this.tenente = tenente;
+        this.capitano = capitano;
+        this.maggiore = maggiore;
+        this.colonnello = colonnello;
+        this.generale = generale;
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+//es 1:
         System.out.println("Nome completo: " + adapter.getNomeCompleto());
         System.out.println("Età: " + adapter.getEta());
 
